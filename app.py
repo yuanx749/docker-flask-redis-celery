ebcd09117e29cb49af7e015ad1ba5b5b685f46a4
yuanx749 <yuanx749@gmail.com>
2019-11-30 19:06:46 +0100

    Auto refresh

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask, redirect, url_for
-from tasks import app_celery
+from flask import Flask, redirect, url_for, render_template
+from celery import Celery
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+# CELERY_BROKER_URL = 'redis://localhost'
+# CELERY_RESULT_BACKEND = 'redis://localhost'
+app_celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 app_flask = Flask(__name__)
 
@@ -12,13 +18,27 @@ def test(n):
     result = app_celery.send_task('tasks.test', args=[n])
     return redirect(url_for('status', task_id=result.id))
 
+@app_flask.route('/mapreduce/<int:n>')
+def mapreduce(n):
+    result = app_celery.send_task('tasks.map_reduce', args=[n])
+    return redirect(url_for('chord_status', task_id=result.id))
+
 @app_flask.route('/status/<string:task_id>')
 def status(task_id):
     result = app_celery.AsyncResult(task_id)
     if result.successful():
-        return result.get()
+        return render_template('status.html', result=result.get())
     else:
-        return result.state
+        return render_template('status.html', result=result.state)
+
+@app_flask.route('/chord_status/<string:task_id>')
+def chord_status(task_id):
+    chord_result = app_celery.AsyncResult(task_id)
+    result = app_celery.AsyncResult(chord_result.get()[0][0])
+    if result.successful():
+        return render_template('status.html', result=result.get())
+    else:
+        return render_template('status.html', result=result.state)
 
 if __name__ == "__main__":
     app_flask.run(host='0.0.0.0', debug=True)
--- tasks.py
+++ tasks.py
@@ -1,8 +1,10 @@
-from celery import Celery
+from celery import Celery, chord, group, chain
 import time
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+# CELERY_BROKER_URL = 'redis://localhost'
+# CELERY_RESULT_BACKEND = 'redis://localhost'
 app_celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 app_celery.conf.worker_prefetch_multiplier = 1
 
@@ -10,6 +12,21 @@ app_celery.conf.worker_prefetch_multiplier = 1
 def test(n):
     time.sleep(n)
     return 'hello'
+
+@app_celery.task()
+def mapper(n):
+    return n ** 2
+
+@app_celery.task()
+def reducer(numbers):
+    return sum(numbers)
+
+@app_celery.task()
+def map_reduce(n):
+    callback = reducer.s()
+    header = [mapper.s(i) for i in range(n)]
+    return chord(header)(callback)
+    # return chain(group(header), callback).apply_async()
 
 if __name__ == "__main__":
     app_celery.start(['celery', 'worker', '-l', 'info'])

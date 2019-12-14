979f8ea202037bf289042b13fa625fc2d2f7de66
yuanx749 <yuanx749@gmail.com>
2019-12-14 14:40:35 +0100

    Mapreduce with chord

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from celery import Celery
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
-# CELERY_BROKER_URL = 'redis://localhost'
-# CELERY_RESULT_BACKEND = 'redis://localhost'
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 app_celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 app_flask = Flask(__name__)
@@ -21,24 +21,26 @@ def test(n):
 @app_flask.route('/mapreduce/<int:n>')
 def mapreduce(n):
     result = app_celery.send_task('tasks.map_reduce', args=[n])
-    return redirect(url_for('chord_status', task_id=result.id))
+    return redirect(url_for('mapreduce_status', task_id=result.id))
 
 @app_flask.route('/status/<string:task_id>')
 def status(task_id):
-    result = app_celery.AsyncResult(task_id)
-    if result.successful():
-        return render_template('status.html', result=result.get())
+    res = app_celery.AsyncResult(task_id)
+    successful=res.successful()
+    if successful:
+        result = res.get()
     else:
-        return render_template('status.html', result=result.state)
+        result = None
+    return render_template('status.html', result=result, successful=successful, state=res.state)
 
-@app_flask.route('/chord_status/<string:task_id>')
-def chord_status(task_id):
+@app_flask.route('/mapreduce_status/<string:task_id>')
+def mapreduce_status(task_id):
     chord_result = app_celery.AsyncResult(task_id)
-    result = app_celery.AsyncResult(chord_result.get()[0][0])
-    if result.successful():
-        return render_template('status.html', result=result.get())
+    if chord_result.successful():
+        reducer_id = chord_result.get()[0][0]
+        return redirect(url_for('status', task_id=reducer_id))
     else:
-        return render_template('status.html', result=result.state)
+        return render_template('status.html', result=None, successful=False, state=chord_result.state)
 
 if __name__ == "__main__":
     app_flask.run(host='0.0.0.0', debug=True)
